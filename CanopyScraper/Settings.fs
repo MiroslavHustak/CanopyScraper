5f8f8c4e84eeafd1def7411709952898ea778805
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-03-08 22:41:16 +0100

    08-03-2026

--- CanopyScraper/Settings.fs
+++ CanopyScraper/Settings.fs
@@ -1,9 +1,16 @@
 ﻿namespace Settings
 
+open System
+
 module Settings = 
 
     let [<Literal>] internal path = @"CanopyResults/canopy_results.json"                
-    let [<Literal>] internal url = @"http://kodis.somee.com/api/" 
+   
+    let internal url =
+        match Environment.GetEnvironmentVariable "DOTNET_RUNNING_IN_CONTAINER" with
+        | "true" -> @"http://canopy-api/" //For Docker networking
+        | _      -> @"http://kodis.somee.com/api/"
+
     let [<Literal>] internal apiKeyTest = "test747646s5d4fvasfd645654asgasga654a6g13a2fg465a4fg4a3"
     let [<Literal>] internal pathToDriver = @"c:/temp/driver"
     let [<Literal>] internal urlKodis = @"https://kodis-files.s3.eu-central-1.amazonaws.com/"
